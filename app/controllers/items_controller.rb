ddbd2cf5fd70a8a614a639efc7598fdd9e81251a
ebarr <erinnlspringer@gmail.com>
2014-02-05 16:31:59 -0600

    Created item model

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,50 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @item = Item.new
+    @item.title = params[:title]
+    @item.price = params[:price]
+    @item.image = params[:image]
+
+    if @item.save
+      redirect_to items_url, notice: "Item created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def update
+    @item = Item.find_by(id: params[:id])
+    @item.title = params[:title]
+    @item.price = params[:price]
+    @item.image = params[:image]
+
+    if @item.save
+      redirect_to items_url, notice: "Item updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find_by(id: params[:id])
+    @item.destroy
+
+    redirect_to items_url, notice: "Item deleted."
+  end
+end
