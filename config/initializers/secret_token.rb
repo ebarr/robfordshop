14c6f091d01e8ff5cb430f019adbc5f0c8134391
ebarr <erinnlspringer@gmail.com>
2014-02-05 13:49:05 -0600

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Robfordshop::Application.config.secret_key_base = '4e11c027baee6ba5a4e583b63c3d2f54ed51c14f05bd7e053b1398efd6c30c733f8c3f7d8668e815599c93bb24b0d9de20d55b19b77e190ca3dcb27e998d3698'
