d2382858eb3c2641d694c774164ced278c6a7cee
Pete Albertson <albertson.peter@gmail.com>
2014-02-06 22:33:04 -0600

    change price column migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206195233) do
+ActiveRecord::Schema.define(version: 20140207043209) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
